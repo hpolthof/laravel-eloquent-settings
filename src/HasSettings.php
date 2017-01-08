d62cef778228e4f986e2db82dbaa1cac918e1b72
hpolthof <0lth0f!>
2017-01-08 19:45:50 +0100

    Added support for columnname changes and defaultValues.

--- src/HasSettings.php
+++ src/HasSettings.php
@@ -6,18 +6,51 @@ trait HasSettings
 {
     protected $__attributes = null;
     protected $settingsField = 'settings';
+    protected $defaultSettings = [];
+    protected $__settingsWithDefaults = null;
 
-    public function getSettingsAttribute($value) {
+    public function __call($method, $parameters)
+    {
+        if($method == 'get'.Str::studly($this->settingsField).'Attribute') {
+            return call_user_func([$this, '__getSettingsAttribute'], $parameters);
+        }
+
+        if($method == 'set'.Str::studly($this->settingsField).'Attribute') {
+            return call_user_func([$this, '__setSettingsAttribute'], $parameters);
+        }
+
+        return parent::__call($method, $parameters);
+    }
+
+    public function hasGetMutator($key)
+    {
+        if($key === $this->settingsField) {
+            return true;
+        }
+        return parent::hasGetMutator($key);
+    }
+
+    public function hasSetMutator($key)
+    {
+        if($key === $this->settingsField) {
+            return true;
+        }
+        return parent::hasSetMutator($key);
+    }
+
+    private function __getSettingsAttribute($value) {
         if($this->__attributes === null) {
             $this->__attributes = [];
             if(strlen($value) > 0) {
                 $this->__attributes = json_decode($value, true);
             }
+
+            $this->populateSettingsArray();
         }
         return $this->__attributes;
     }
 
-    public function setSettingsAttribute($value) {
+    private function __setSettingsAttribute($value) {
         $this->attributes[$this->settingsField] = json_encode($value);
     }
 
@@ -28,7 +61,7 @@ trait HasSettings
      */
     public function getSetting($name, $default = null) {
         if($this->hasSetting($name)) {
-            return $this->{$this->settingsField}[$name];
+            return $this->__settingsWithDefaults[$name];
         }
         return $default;
     }
@@ -42,6 +75,7 @@ trait HasSettings
         $this->hasSetting($name);
         $this->__attributes[$name] = $value;
         $this->{$this->settingsField} = $this->__attributes;
+        $this->populateSettingsArray();
         return $this;
     }
 
@@ -50,7 +84,7 @@ trait HasSettings
      * @return bool
      */
     public function hasSetting($name) {
-        return array_key_exists($name, $this->{$this->settingsField});
+        return array_key_exists($name, $this->__settingsWithDefaults);
     }
 
     /**
@@ -58,6 +92,45 @@ trait HasSettings
      */
     public function getSettingsCollection()
     {
-        return new Collection($this->{$this->settingsField});
+        return new Collection($this->__settingsWithDefaults);
+    }
+
+    /**
+     * @return array
+     */
+    protected function mergeDefaultSettings()
+    {
+        $flatSettings = array_dot($this->{$this->settingsField});
+        $flatDefaults = array_dot($this->defaultSettings);
+
+        foreach($flatDefaults as $k => $v) {
+            $flatSettings[$k] = $v;
+        }
+
+        $result = [];
+        foreach($flatSettings as $k => $v) {
+            array_set($result, $k, $v);
+        }
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSettingsArray()
+    {
+        if(count($this->defaultSettings) > 0) {
+            return $this->mergeDefaultSettings();
+        }
+        return $this->{$this->settingsField};
+    }
+
+    /**
+     * @return $this
+     */
+    protected function populateSettingsArray()
+    {
+        $this->__settingsWithDefaults = $this->getSettingsArray();
+        return $this;
     }
 }
